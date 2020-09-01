47b63439129deca87f53f74eaa96cfa4ec95385b
peterix <fork@kuelzer.me>
2020-09-01 21:52:43 +0200

    add tracelog to start and removed eventhandler because this will be done by tracelog

--- src/DebugMod.cs
+++ src/DebugMod.cs
@@ -15,30 +15,16 @@ namespace DebugMod
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            api.Server.Logger.EntryAdded += OnServerLogEntry;
             api.RegisterCommand(new HungerDebugCommand(api));
             api.RegisterCommand(new ModListServerCommand(api));
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
-            api.World.Logger.EntryAdded += OnClientLogEntry;
             api.RegisterCommand(new DebugTreeCommand(api));
             api.RegisterCommand(new LangMatchDebugCommand(api));
             api.RegisterCommand(new LangListDebugCommand(api));
             api.RegisterCommand(new ModListClientCommand(api));
-        }
-
-        private void OnServerLogEntry(EnumLogType logType, string message, params object[] args)
-        {
-            if (logType == EnumLogType.VerboseDebug) return;
-            Debug.WriteLine("[Server " + logType + "] " + message, args);
-        }
-
-        private void OnClientLogEntry(EnumLogType logType, string message, params object[] args)
-        {
-            if (logType == EnumLogType.VerboseDebug) return;
-            Debug.WriteLine("[Client " + logType + "] " + message, args);
         }
     }
 }
